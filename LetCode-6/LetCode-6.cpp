6ace0486ad9f6daa244d25e644b258548f053527
Anikalogika4417 <anikalogika4417@gmail.com>
2023-11-04 20:08:09 +0200

    2nd

--- LetCode-6/LetCode-6.cpp
+++ LetCode-6/LetCode-6.cpp
@@ -7,30 +7,36 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1) return s;
-
         vector<string> result(numRows);
         bool is_straight = true;
-        int index = 0;
-        for (int i = 0; i < s.size(); i++) {
-            result[index] = result[index] + s[i];
-            index = is_straight ? index+1 : index-1;
-
-            if (i == 0) continue;
-            if (index <= 0 || index >= numRows) {
-                index = is_straight ? numRows - 2 : 0;
-                is_straight = !is_straight;
-                if (numRows == 2) {
-                    is_straight = !is_straight;
+        for (int i = 0; i < s.size();) {
+            if (is_straight) {
+                for (int j = 0; j < numRows; j++) {
+                    result[j] = result[j] + s[i];
+                    i++;
+                    if (i == s.size())
+                    {
+                        break;
+                    }
                 }
             }
+            else {
+                for (int j = numRows - 2; j > 0; j--) {
+                    result[j] = result[j] + s[i];
+                    i++;
+                    if (i == s.size())
+                    {
+                        break;
+                    }
+                }
+            }
+            is_straight = !is_straight;
         }
         string res;
         for (auto i : result) res = res + i;
         return  res;
     }
 };
-
 int main()
 {
     Solution test;
